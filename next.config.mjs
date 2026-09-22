033a19794ba6f388d246b90cbb6f219d69894c6d
IlyasKalamullah <ilyaskalamullahh@gmail.com>
2026-09-23 02:00:15 +0700

    Fix: sertakan file Prisma di deploy Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,9 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   experimental: { serverActions: { bodySizeLimit: "5mb" } },
+  // Pastikan file mesin Prisma (.wasm) ikut ter-deploy ke server Vercel
+  outputFileTracingIncludes: {
+    "/**/*": ["./node_modules/.prisma/client/**/*", "./node_modules/@prisma/client/runtime/**/*"],
+  },
 };
 export default nextConfig;
